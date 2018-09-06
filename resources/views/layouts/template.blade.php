c6e9f1bafb40ee4a5e1859b5f19ebd4931313f5b
admin <ossimplementers@gmail.com>
2018-09-06 13:34:04 +0530

    ....

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -18,88 +18,10 @@
     <div class="container">
 
         @include('pages.include.top_head')
-        
 
-        <header>
-            <div class="center-container">
-                <div class="logo"><img src="{{ asset('build/assets/images/logo.jpg') }}" alt="Interior Xpression"/></div>
-                <div class="main-menu">
-                    <ul>
-                        <a href="#">
-                            <li>
-                                <h4>OUR RANGE</h4>
-                            </li>
-                        </a>
-                        <a href="#">
-                            <li>
-                                <h4>ABOUT US</h4>
-                            </li>
-                        </a>
-                        <a href="#">
-                            <li>
-                                <h4>WALLPAPER INSTALLER</h4>
-                            </li>
-                        </a>
-                        <a href="#">
-                            <li>
-                                <h4>HOW TO MEASURE</h4>
-                            </li>
-                        </a>
-                        <a href="#">
-                            <li>
-                                <h4>OFFERS</h4>
-                            </li>
-                        </a>
-                        <a href="#">
-                            <li>
-                                <h4>CONTACT US</h4>
-                            </li>
-                        </a>
-                    </ul>
-                </div>
-                <div class="main-menu2">
-                    <ul>
-                        <a href="#">
-                            <li>
-                                <h4>OUR RANGE</h4>
-                            </li>
-                        </a>
-                        <a href="#">
-                            <li>
-                                <h4>ABOUT US</h4>
-                            </li>
-                        </a>
-                        <a href="#">
-                            <li>
-                                <h4>WALLPAPER INSTALLER</h4>
-                            </li>
-                        </a>
-                        <a href="#">
-                            <li>
-                                <h4>HOW TO MEASURE</h4>
-                            </li>
-                        </a>
-                        <a href="#">
-                            <li>
-                                <h4>OFFERS</h4>
-                            </li>
-                        </a>
-                        <a href="#">
-                            <li>
-                                <h4>CONTACT US</h4>
-                            </li>
-                        </a>
-                    </ul>
-                </div>
-                <div class="menu-icons">
-                    <ul>
-                        <li><i class="fas fa-heart"></i></li>
-                        <li><i class="fa fa-search"></i></li>
-                        <li><i class="fa fa-shopping-cart"></i></li>
-                        <li><i class="fa fa-user"></i></li>
-                    </ul>
-                </div>
-            </header>
+          @include('pages.include.header')
+    
+        
             <div class="home-banner"> </div>
             <div class="seller-sec">
                 <div class="seller-top">
