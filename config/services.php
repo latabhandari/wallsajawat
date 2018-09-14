61c18bbc64f399dce4fa75dfe843712a1a500397
admin <ossimplementers@gmail.com>
2018-09-14 15:08:11 +0530

    ...

--- config/services.php
+++ config/services.php
@@ -38,13 +38,13 @@ return [
     'facebook' => [
        'client_id' => env('FACEBOOK_CLIENT_ID', ''),         // Your Facebook Client ID
        'client_secret' => env('FACEBOOK_CLIENT_SECRET', ''), // Your Facebook Client Secret
-       'redirect' => 'http://wallsajawat.com/beta/login/facebook/callback',
+       'redirect' => env('FACEBOOK_RETURN_URL', '')
     ],
 
     'google' => [
        'client_id' => env('GOOGLE_CLIENT_ID', ''),         // Your Google Client ID
        'client_secret' => env('GOOGLE_CLIENT_SECRET', ''), // Your Google Client Secret
-       'redirect' => 'http://wallsajawat.com/beta/login/google/callback',
+       'redirect' => env('GOOGLE_RETURN_URL', '')
     ]
 
 ];
