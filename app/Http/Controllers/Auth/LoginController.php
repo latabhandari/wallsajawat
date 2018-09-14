a6cbfc7365776763c47715d29deecaf5faa122a3
admin <ossimplementers@gmail.com>
2018-09-14 15:12:51 +0530

    ....

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,6 +39,42 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function redirectToProvider($provider)
+     {
+        return Socialite::driver($provider)->redirect();
+     }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback($provider)
+     {
+        $user = Socialite::driver($provider)->user();
+
+        $authUser = $this->findOrCreateUser($user, $provider);
+        Auth::login($authUser, true);
+        return redirect($this->redirectTo);
+        // $user->token;
+     }
+
+    public function findOrCreateUser($user, $provider)
+     {
+        $authUser = User::where('provider_id', $user->id)->first();
+        if ($authUser)
+        return $authUser;
+
+        return User::create(['name' => $user->name, 'email' => $user->email, 'provider' => $provider, 'provider_id' => $user->id, 'unix_timestamp' => time()]);
+     }
+
     protected function authenticated(Request $request, $user)
       {
             return redirect()->route('home.index');
