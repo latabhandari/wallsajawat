c4ddfe26599ec621825313d2eb24f78e1bda86e0
admin <ossimplementers@gmail.com>
2018-09-15 17:30:50 +0530

    ....

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -77,13 +77,6 @@
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" />
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="error" role="alert">
-                                        {{ $errors->first('password_confirmation') }}
-                                    </span>
-                                @endif
-
                             </div>
                         </div>
 
