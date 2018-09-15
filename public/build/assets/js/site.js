9e654a92930d6e85fce53265e5563979dc59216d
admin <ossimplementers@gmail.com>
2018-09-15 13:18:10 +0530

    ...

--- public/build/assets/js/site.js
+++ public/build/assets/js/site.js
@@ -1,5 +1,5 @@
 if (typeof WallSajawat == 'undefined') { WallSajawat = {}; };
 
-WallSajawat.getSitePath    =  function(path) { return 'http://wallsajawat.com/beta/' + path ; };
-WallSajawat.getImages      =  function(file) { return 'http://wallsajawat.com/assets/images/' + file ; };
+WallSajawat.getSitePath    =  function(path) { return 'https://wallsajawat.com/beta/' + path ; };
+WallSajawat.getImages      =  function(file) { return 'https://wallsajawat.com/assets/images/' + file ; };
 
