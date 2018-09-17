e4333f6265ad41a7cc12c55bbd4593fa06e0baee
admin <ossimplementers@gmail.com>
2018-09-17 10:46:02 +0530

    ......

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -35,7 +35,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/beta';
 
     /**
      * Create a new controller instance.
