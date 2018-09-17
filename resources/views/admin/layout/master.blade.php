592cdf5bdb7b9874b91e4fd1b348bf1211f8f685
admin <ossimplementers@gmail.com>
2018-09-17 10:23:20 +0530

    ....

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -57,7 +57,7 @@
             <div class="pull-right hidden-xs">
                <b>Version</b> 1.0
             </div>
-            <strong>Copyright &copy; {{ date('Y') }} All rights reserved.
+            <strong>Copyright &copy; {{ date('Y') }} All rights reserved.</strong>
          </footer>
          <!-- Control Sidebar -->
          
