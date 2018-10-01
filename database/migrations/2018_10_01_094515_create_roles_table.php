99f68d60f3d916b6318dc3b87c60004774a9f771
latabhandari <latabhandari95@gmail.com>
2018-09-30 21:21:35 -0700

    Add existing file

---
+++ database/migrations/2018_10_01_094515_create_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id')->comment('Id');
+            $table->string('name')->comment('Name');
+            $table->text('permission')->comment('Permission')->nullable();
+            $table->integer('created_at_timestamp')->comment('Created TimeStamp')->nullable();
+            $table->integer('updated_at_timestamp')->comment('Updated TimeStamp')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
