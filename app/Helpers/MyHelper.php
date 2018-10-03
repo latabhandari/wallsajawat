df31e84c578deadc592e394657af1272628df99c
admin <ossimplementers@gmail.com>
2018-10-03 10:18:28 +0530

    ...

--- app/Helpers/MyHelper.php
+++ app/Helpers/MyHelper.php
@@ -17,7 +17,7 @@ class MyHelper
 		  public static function getPermission($index = '') 
 			 {
 			  	  $userinfo  =  Auth::user();
-			  	  print_r($userinfo);
+			  	  print_r($userinfo); die;
 			  	  $role_id   =  DB::table('users')->select('is_admin')->where('id', $userinfo->id)->first()->is_admin;
 
 			  	  if ($role_id == 1)
