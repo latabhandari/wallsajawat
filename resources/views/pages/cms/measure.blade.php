4932246b4fcdd2e89afc0f7def393e1eacc2a091
admin <ossimplementers@gmail.com>
2018-10-04 16:57:46 +0530

    ...

--- resources/views/pages/cms/measure.blade.php
+++ resources/views/pages/cms/measure.blade.php
@@ -2,7 +2,56 @@
 
 @section('content')
 
-<div class="container" style="float: left;line-height: 26px;margin:10px 0">
-     <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12 text-center">
+			<div class="title measure-title">
+				<h3>How to Measure</h3>
+				<ul class="navbar">
+					<li class="nobackground"><a href=""><i class="fa fa-home" aria-hidden="true"></i></a></li>
+					<li><a href="">How it works</a></li>
+					<li><a href="">How to Measure</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-sm-12">
+			<div class="measure-content">
+				<p>Size does matter when it comes to buying wallpaper for your home or office because:</p>
+				
+				<ul>
+				<li>You would not want to buy extra rolls which have no use and lie stacked around.</li>
+				<li>You do not want to fall short of rolls to cover your entire wall.</li>
+				<li>You do not want tacky work with frayed wallpaper sticking out at the corners.</li>
+				</ul>
+				
+				<p>Therefore, once you have zeroed down on the design, the next step is to measure the size of wallpaper you would require to cover up your walls in an aesthetic way.</p>
+				
+				<p>In order to measure your wallpaper, you have to consider what you require:</p>
+				
+				<ul>
+				<li>Full wall covering for high impact and best value for money.</li>
+				<li>Partial wall covering for adding a certain theme to smaller spaces.</li>
+				</ul>
+				
+				<p></p><h4>For Full Wall Coverings</h4><p></p>
+				<ul>
+				<li><b>Width</b> - Measure the horizontal distance of the wall, from corner to corner.</li>
+				<li><b>Height</b>&nbsp;– Measure the vertical height at each corner of the wall. Some walls are not perfectly square. If there is a difference in height, always select the greater of the two height measurements.</li>
+				</ul>
+				<p class="text-center"> <img src="Images/width-height.png" alt=""></p>
+				
+				<p></p><h4>For Partial Wall Coverings</h4><p></p>
+				<p>Make registration marks along the area that you want to cover.</p>
+				
+				<ul>
+				<li><b>Width</b> - Measure the horizontal distance between registration marks.</li>
+				<li><b>Height</b>&nbsp;– Measure the vertical height at each corner of the registration mark. You may want to cover a variety of heights. In case of difference in height, always select the greater of the two height measurements.</li>
+				</ul>
+				<p class="text-center"> <img src="Images/width-height1.png" alt=""></p>
+				<p>Most wallpaper designs are pretty flexible and trimming the edges to achieve a perfect finish does not harm the overall look. Always use a measuring tape to get the most accurate measurement. Finally, add a few extra inches along the sides of your wallpaper. This will give you a little breathing space in case of any odd mistakes!
+				Got any queries? Simply call us </p>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
