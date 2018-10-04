53a47472227ff111a0948956e2427daacae67e34
admin <ossimplementers@gmail.com>
2018-10-04 15:13:30 +0530

    ...

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,8 +30,22 @@
         
         @include('pages.include.footer')
 
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-		<script type="text/javascript" src="{{ asset('build/assets/js/site.js') }}"></script>
+        <script src="{{ asset('build/assets/js/jquery-3.2.1.js') }}" type="text/javascript"></script>
+	    <script src="{{ asset('build/assets/js/bootstrap.min.js') }}" type="text/javascript"></script>
+	    <script src="{{ asset('build/assets/js/jquery.bxslider.min.js') }}" type="text/javascript"></script>
+	    <script>
+	      $(document).ready(function() {
+	             $('.bxslider').bxSlider({
+	                                            minSlides: 1,
+	                                            maxSlides: 3,
+	                                            slideWidth: 400,
+	                                            slideMargin: 30,
+	                                            pager: true,
+	                                            auto: true,
+	                                            infiniteLoop: true
+	                                    });
+	       });
+	    </script>
     
 </body>
 </html>
