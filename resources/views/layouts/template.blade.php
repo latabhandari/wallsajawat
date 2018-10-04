6b65338b3ae0f2724e53e0c60ea08a7094c8fc18
admin <ossimplementers@gmail.com>
2018-10-04 15:12:13 +0530

    ....

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -13,7 +13,6 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 <link href="{{ asset('build/assets/css/style.css') }}" rel="stylesheet" type="text/css">
-
 <script type="text/javascript" src="{{ asset('build/assets/js/site.js') }}"></script>
 
 </head>
