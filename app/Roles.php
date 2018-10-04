57aed6a47fcabd26562ddb58655ae5380060dd22
latabhandari <latabhandari95@gmail.com>
2018-10-03 21:17:01 -0700

    Add existing file

---
+++ app/Roles.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Roles extends Model
+{
+    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'permission', 'created_at_timestamp', 'updated_at_timestamp'
+    ];
+
+}
