fb3cbe9cebb68a980331a64761bef06abe5da334
admin <ossimplementers@gmail.com>
2018-10-04 13:18:20 +0530

    ...

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -7,14 +7,15 @@
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta content="width=device-width,initial-scale=1.0" name="viewport">
+
 <link href="{{ asset('build/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
 <link href="{{ asset('build/assets/css/jquery.bxslider.min.css') }}" rel="stylesheet" type="text/css">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" />
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
-<link href="{{ asset('build/assets/css/style.css') }}" rel="stylesheet" type="text/css">
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<link href="{{ asset('build/assets/css/stylesheet.css') }}" rel="stylesheet" type="text/css">
+
 <script type="text/javascript" src="{{ asset('build/assets/js/site.js') }}"></script>
+
 </head>
 <body>
 
