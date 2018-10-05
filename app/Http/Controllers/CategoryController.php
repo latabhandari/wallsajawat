03d6fc732d09be323ccd5fcc48311c634bf32302
admin <ossimplementers@gmail.com>
2018-10-05 13:22:31 +0530

    ...

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,27 +11,11 @@ class CategoryController extends Controller
     //
     public function product($slug)
       {          
-      	echo $slug;
-      	dd(Categories::where('slug', $slug)->firstOrFail());
           $category_info  =  Categories::where('slug', $slug)->firstOrFail();
-
-print_r($category_info);
-
-
-
-DB::enableQueryLog();
-          dd(DB::getQueryLog());
-
-
-	     // print_r($category_info); die;
           $products = DB::table('products')
 						->join('product_categories', 'products.id', '=', 'product_categories.product_id')
-			            ->where('category_id', 1)
+			            ->where('category_id', $category_info->id)
 			            ->get();
-
-			            dd(DB::getQueryLog());
-
-
           return view('pages.category.product', compact('products'));
       }
 }
