1bacf55871bf9a02a557019183dbbd036bc3e085
admin <ossimplementers@gmail.com>
2018-10-06 10:39:21 +0530

    ....

--- app/Dimension.php
+++ app/Dimension.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Dimension extends Model
 {
     //
+    protected $table    = 'dimensions';
+	
+	protected $fillable = ['id', 'name', 'created_at_timestamp', 'updated_at_timestamp'];
 }
