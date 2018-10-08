de190276ce71b261b4778c6fc5827583cd1a7ffe
admin <ossimplementers@gmail.com>
2018-10-08 11:52:41 +0530

    .....

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,9 +1,31 @@
 @extends('layouts.master')
 
+@section('top_yield')
+<link href="{{ asset('build/assets/css/easyzoom.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('build/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('build/assets/css/jquery.bxslider.min.css') }}" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+<link href="{{ asset('build/assets/css/style.css') }}" rel="stylesheet" type="text/css">
+<script src="{{ asset('build/assets/js/jquery-3.2.1.js') }}" type="text/javascript"></script>
+<script type="text/javascript" src="{{ asset('build/assets/js/site.js') }}"></script>
+@endsection
+
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+
+        <div class="col-sm-12 text-center">
+            <div class="title measure-title">
+                <ul class="navbar">
+                    <li class="nobackground"><a href="{{ route('home.index') }}"><i class="fa fa-home" aria-hidden="true"></i></a></li>
+                    <li>Login</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-sm-8">
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
                 <div class="card-body">
@@ -41,6 +63,8 @@
                 </div>
             </div>
         </div>
+
+        <div class="col-sm-4"></div>
     </div>
 </div>
 @endsection
