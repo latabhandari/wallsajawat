e63663a04b50e456b03852d1b26df6188b3962ed
admin <ossimplementers@gmail.com>
2018-10-08 11:48:28 +0530

    ...

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -87,7 +87,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-success">
+                                <input type="submit" class="btn btn-block">
                                     {{ __('Login') }}
                                 </button>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -113,7 +113,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <input type="submit" class="btn btn-block">
                                     {{ __('Register') }}
                                 </button>
                             </div>
