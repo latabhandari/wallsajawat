d867bcfb0d7572cffd099efa7131e61c61d68adc
admin <ossimplementers@gmail.com>
2018-10-10 13:00:45 +0530

    ...

--- app/Wishlist.php
+++ app/Wishlist.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Wishlist extends Model
 {
     //
+    protected $table = 'wishlist';
+	
+	protected $fillable = ['id', 'name'];
 }
--- database/migrations/2018_10_10_125144_create_wishlists_table.php
+++ database/migrations/2018_10_10_125144_create_wishlists_table.php
@@ -14,8 +14,16 @@ class CreateWishlistsTable extends Migration
     public function up()
     {
         Schema::create('wishlists', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->comment('Id');
+            $table->unsignedInteger('user_id')->comment('User Id');
+            $table->unsignedInteger('pid')->comment('Product Id');
+            $table->unsignedInteger('unix_timestamp')->comment('Unix Timestamp');
+            $table->string('ip_address')->comment('Ip Address')->nullable();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('pid')->references('id')->on('products');
+            
         });
     }
 
