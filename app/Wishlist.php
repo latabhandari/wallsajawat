a8643baad3fcb54adf81e67b135510965521dfdb
admin <ossimplementers@gmail.com>
2018-10-10 13:01:32 +0530

    ...

--- app/Wishlist.php
+++ app/Wishlist.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Wishlist extends Model
 {
     //
-    protected $table = 'wishlist';
+    protected $table    = 'wishlists';
 	
-	protected $fillable = ['id', 'name'];
+	protected $fillable = ['id', 'user_id', 'pid', 'unix_timestamp', 'ip_address'];
 }
