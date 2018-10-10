e9c516f46c29314439f913b7dcc01bb86b488222
admin <ossimplementers@gmail.com>
2018-10-10 13:24:50 +0530

    .....

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,17 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Auth;
+
 class ProfileController extends Controller
 {
     //
+    private $_userid;
 
      public function __construct()
 	    {
 	        $this->middleware('auth');
+	        $this->_userid = Auth::user()->id;
 	    }
 
      public function mywishlist()
        {
+        	$users = DB::table('wishlists')
+		            			->join('products', 'wishlists.pid', '=', 'products.id')
+		            			->select('products.*')
+		           		 		->where('wishlists.user_id', $this->_userid)
+		           				->get();
+		    print_r($users);
+
            return view('pages.wishlist.wishlist');
        }
 }
