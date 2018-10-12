55cecd1ef1fa6220d7783ae01af9cc6fb868f100
admin <ossimplementers@gmail.com>
2018-10-12 14:21:34 +0530

    ...

--- app/Order.php
+++ app/Order.php
@@ -8,5 +8,5 @@ class Order extends Model
 {
     //
     protected $table    = 'orders';
-    protected $fillable = ['order_number', 'user_id', 'coupon', 'discount', 'total_amount', 'payable_amount', 'ip_address', 'user_agent', 'unix_timestamp'];
+    protected $fillable = ['order_number', 'user_id', 'coupon', 'discount', 'total_amount', 'payable_amount', 'ip_address', 'user_agent', 'unix_timestamp', 'shipping_address'];
 }
