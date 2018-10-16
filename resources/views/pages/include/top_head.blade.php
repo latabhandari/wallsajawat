89a6ea3b6d8e76f56239464a5441791d4e1ee114
admin <ossimplementers@gmail.com>
2018-10-16 09:37:55 +0530

    .....

--- resources/views/pages/include/top_head.blade.php
+++ resources/views/pages/include/top_head.blade.php
@@ -9,11 +9,11 @@
                    <span><i class="fa fa-map-marker"></i>
                      @guest 
                       <a href="{{ route('login') }}">{{ __('Login') }}</a>
-                      &nbsp;<a href="{{ route('register') }}">{{ __('Register') }}</a>
+                      &nbsp;|&nbsp;<a href="{{ route('register') }}">{{ __('Register') }}</a>
                      @endguest
                     </span> 
-                   <span><i class="fa fa-phone"></i><a href="tel:0123456789">+91 0123456789</a></span>
-                   <span><i class="fa fa-envelope"></i><a href="mailto:info@wallsajawat.com">info@wallsajawat.com</a></span>
+                   <span><i class="fa fa-phone"></i><a href="tel:0123456789">{{ __('+91 0123456789') }}</a></span>
+                   <span><i class="fa fa-envelope"></i><a href="mailto:info@wallsajawat.com">{{ __('info@wallsajawat.com') }}</a></span>
                 </div>
               </div>
             </div>
