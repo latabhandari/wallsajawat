5109e0820fd3ab6c1d37885a23c26408f51feeba
admin <ossimplementers@gmail.com>
2018-10-16 12:15:37 +0530

    ...

--- resources/views/admin/pages/category/index.blade.php
+++ resources/views/admin/pages/category/index.blade.php
@@ -46,7 +46,6 @@
                   <th>S. No</th>
                   <th>Name</th>
                   <th>Slug</th>
-                  <th>Position</th>
                   <th>Title</th>
                   <th>Status</th>
                   <th>Action</th>
@@ -63,7 +62,6 @@
                   <td>{{ ++$i }}</td>
                   <td>{!! MyHelper::showCategories($data->id) !!}</td>
                   <td>{{ $data->slug }}</td>
-                  <td>{{ $data->wallpaper_pos }}</td>
                   <td>{{ $data->page_title }}</td>
                   <td><img src="{{ URL::asset('backend/assets/images/'.$status_img) }}" /></td>
                   <td>
@@ -80,7 +78,7 @@
 
                      @php
                         $destroy_category = MyHelper::getPermission('destroy_category');
-                        if ( ! empty($destroy_category)) {
+                        if ( ! empty($destroy_category) && $data->id > 5) {
                      @endphp
 
                      {!! Form::open(['style' => 'display:inline', 'method' => 'DELETE', 'route' => ['categories.destroy', $data->id]]) !!}
