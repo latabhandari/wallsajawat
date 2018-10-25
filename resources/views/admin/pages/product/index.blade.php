3ce955de70239e12d47ad70232cd53a48960599e
admin <ossimplementers@gmail.com>
2018-10-25 13:21:47 +0530

    ...

--- resources/views/admin/pages/product/index.blade.php
+++ resources/views/admin/pages/product/index.blade.php
@@ -59,11 +59,12 @@
                   @php
                     $status_img   =  ($data->status == 1) ? "bullet-green.png" : "bullet-red.png";
                     $categoryinfo =  MyHelper::getCategoryInfoById($data->id, ['name']);
+                    print_r($categoryinfo);
                   @endphp
 
                     <tr>
                       <td>{{ ++$i }}</td>
-                      <td>{{ $categoryinfo['name'] }}</td>
+                      
                       <td>{{ $data->name }}</td>
                       <td>{{ $data->sku }}</td>
                       <td>Rs. {{ $data->price }}</td>
