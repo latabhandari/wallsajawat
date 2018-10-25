3ef73eb79ff6aa71ea5f2424156edb91cc12cab1
admin <ossimplementers@gmail.com>
2018-10-25 13:26:49 +0530

    ...

--- resources/views/admin/pages/product/index.blade.php
+++ resources/views/admin/pages/product/index.blade.php
@@ -68,7 +68,7 @@
                       <td>{{ $data->name }}</td>
                       <td>{{ $data->sku }}</td>
                       <td>Rs. {{ $data->price }}</td>
-                      <td>Rs. {{ $data->title }}</td>
+                      <td>{{ $data->page_title }}</td>
                       <td><img src="{{ URL::asset('backend/assets/images/'.$status_img) }}" /></td>
                       <td>
                         @php
