6e016b81a197e8963c6623895cb32116a6519dfd
admin <ossimplementers@gmail.com>
2018-10-26 13:31:59 +0530

    ...

--- resources/views/pages/wishlist/wishlist.blade.php
+++ resources/views/pages/wishlist/wishlist.blade.php
@@ -12,6 +12,9 @@
 <link href="{{ asset('build/assets/css/style.css') }}" rel="stylesheet" type="text/css">
 <script src="{{ asset('build/assets/js/jquery-3.2.1.js') }}" type="text/javascript"></script>
 <script type="text/javascript" src="{{ asset('build/assets/js/site.js') }}"></script>
+<style>
+  .imgb img {width:120px;height:120px}
+</style>
 @endsection
 
 @section('content')
