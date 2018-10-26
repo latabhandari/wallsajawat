58eef9c48d66fd4a6cefb68635ef4a2748af3f66
admin <ossimplementers@gmail.com>
2018-10-26 10:22:23 +0530

    .....

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -16,6 +16,9 @@
 <script type="text/javascript" src="{{ asset('build/assets/js/site.js') }}"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"></script>
+<style>
+    .error{font-weight:normal}
+</style>
 </head>
 <body>
 
