21c5908944f1751cece34337a3a7827f06770d6b
admin <ossimplementers@gmail.com>
2018-10-26 17:11:08 +0530

    ...

--- resources/views/admin/include/inc_header.blade.php
+++ resources/views/admin/include/inc_header.blade.php
@@ -2,9 +2,9 @@
    <!-- Logo -->
    <a href="{{ route('admin.dashboard') }}" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini" style="text-align: left;">wall skin</span>
+      <span class="logo-mini" style="text-align: left;">wallsajawat</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg" style="text-align: left;">wall skin</span>
+      <span class="logo-lg" style="text-align: left;">wallsajawat</span>
    </a>
    <!-- Header Navbar: style can be found in header.less -->
    <!--<nav class="navbar navbar-static-top">
