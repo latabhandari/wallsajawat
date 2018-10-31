a42547d8bb5fc266ff30b2b0a2b7c9c521e5910f
admin <ossimplementers@gmail.com>
2018-10-31 13:19:30 +0530

    ....

--- resources/views/pages/product/detail.blade.php
+++ resources/views/pages/product/detail.blade.php
@@ -388,7 +388,7 @@
                                        },
 
                           submitHandler: function()  {
-                                                            let rating =  $('input[name=rating]:checked', '#ratingfrm').val();
+                                                            let rating =  $('input[name=start]:checked', '#ratingfrm').val();
                                                             let review =  $('#review').val();
                                                             $("#sbmtrt").attr({'disabled': true, 'value': '...'});
 
