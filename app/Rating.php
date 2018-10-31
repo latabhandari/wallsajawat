6366a68b0d2918eadf074dea7c9a43fe59be012c
admin <ossimplementers@gmail.com>
2018-10-31 12:11:52 +0530

    ...

--- app/Rating.php
+++ app/Rating.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     //
+    protected $fillable = [
+        'order_number', 'product_id', 'user_id', 'rating', 'review', 'ip', 'user_agent', 'timestamp'
+    ];
+
 }
