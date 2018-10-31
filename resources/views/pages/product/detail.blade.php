59227f0049c5bf2ac3261744b49756d55128091d
admin <ossimplementers@gmail.com>
2018-10-31 13:12:37 +0530

    ....

--- resources/views/pages/product/detail.blade.php
+++ resources/views/pages/product/detail.blade.php
@@ -241,21 +241,20 @@
                           <i class="fa fa-star-o"></i>
                           <i class="fa fa-star-o"></i>
                           <i class="fa fa-star-o"></i>-->
-                          <select name="star" id="star">
+                          <!--<select name="star" id="star">
                              <option value="1">1 star</option>
                              <option value="2">2 star</option>
                              <option value="3">3 star</option>
                              <option value="4">4 star</option>
                              <option value="5">5 star</option>
-                          </select>
+                          </select>-->
 
                           <fieldset class="rating">
-                              <legend>Please rate:</legend>
-                              <input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Rocks!">5 stars</label>
-                              <input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Pretty good">4 stars</label>
-                              <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label>
-                              <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
-                              <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Sucks big time">1 star</label>
+                              <input type="radio" id="star5" name="star" value="5" /><label for="star5" title="">5 stars</label>
+                              <input type="radio" id="star4" name="star" value="4" /><label for="star4" title="">4 stars</label>
+                              <input type="radio" id="star3" name="star" value="3" /><label for="star3" title="">3 stars</label>
+                              <input type="radio" id="star2" name="star" value="2" /><label for="star2" title="">2 stars</label>
+                              <input type="radio" id="star1" name="star" value="1" /><label for="star1" title="">1 star</label>
                           </fieldset>
 
                         </div>
@@ -389,7 +388,7 @@
                                        },
 
                           submitHandler: function()  {
-                                                            let rating =  $('#star :selected').val();
+                                                            let rating =  $('input[name=rating]:checked', '#ratingfrm').val();
                                                             let review =  $('#review').val();
                                                             $("#sbmtrt").attr({'disabled': true, 'value': '...'});
 
