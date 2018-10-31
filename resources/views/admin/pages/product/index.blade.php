20556ff1ee458829e1610375c40977678e0179f9
admin <ossimplementers@gmail.com>
2018-10-31 10:04:42 +0530

    ....

--- resources/views/admin/pages/product/index.blade.php
+++ resources/views/admin/pages/product/index.blade.php
@@ -73,12 +73,13 @@
                     $status_img   =  ($data->status == 1) ? "bullet-green.png" : "bullet-red.png";
                     $status_msg   =  ($data->status == 1) ? "inactive" : "active";
                     $categoryinfo =  MyHelper::getCategoryInfoById($data->id, ['name']);
+                    print_r($categoryinfo);
 
                   @endphp
 
                     <tr>
                       <td>{{ ++$i }}</td>
-                      <td>{{ $categoryinfo[0]['name'] }}</td>
+                      <td>fsdf</td>
                       <td>{{ $data->name }}</td>
                       <td>{{ $data->sku }}</td>
                       <td>Rs. {{ $data->price }}</td>
