3921f0d93cea25cd4f3450af1b130b081d93fcc8
admin <ossimplementers@gmail.com>
2018-11-01 12:05:39 +0530

    ...

--- app/Helpers/MyHelper.php
+++ app/Helpers/MyHelper.php
@@ -25,7 +25,7 @@ class MyHelper
 				//$rating = Rating::where(['product_id' => $product_id])->sum('rating');
 
 				$table  = env('DB_PREFIX', '') . (new Rating())->getTable();
-				$query  = DB::select('SELECT count(*) as count, sum(`rating`) FROM ' . $table. ' WHERE product_id = ?', array($id));
+				$query  = DB::select('SELECT count(*) as count, sum(`rating`) as rating FROM ' . $table. ' WHERE product_id = ?', array($id))->row();
 
 				print_r($query); die;
    	     }
