bf4327d45eb9f8176589854a17c8ac1d93d7e4c0
admin <ossimplementers@gmail.com>
2018-11-01 10:15:36 +0530

    ....

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,7 +63,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
           'name' => 'required|string|max:255',
           'email' => 'required|string|email|max:255|unique:users',
-          'password' => 'required|string|min:6|regex:^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!?]).*$|confirmed',
+          'password' => 'required|string|min:6|regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()+]+.*)[0-9a-zA-Z\w!@#$%^&*()+]{6,}$/|confirmed',
           'g-recaptcha-response' => 'required|captcha'
         ],  $messages);
     }
