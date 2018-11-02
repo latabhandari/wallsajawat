79301c1e71cfafc83e4d3ffcb9aa4750d1fb6f37
admin <ossimplementers@gmail.com>
2018-11-02 12:38:28 +0530

    ....

--- resources/views/admin/pages/orders/show.blade.php
+++ resources/views/admin/pages/orders/show.blade.php
@@ -57,13 +57,13 @@ ul li{list-style-type:none;padding-left:10px}
                           @endforeach
                         @endif
                             <tr>
-                              <td colspan="5" align="right">Sub Total: Rs. {{ $order->total_amount }}</td>
+                              <td colspan="5" align="left">Sub Total: Rs. {{ $order->total_amount }}</td>
                             </tr>
                             <tr>
-                              <td colspan="5" align="right">Discount: Rs. {{ $order->discount ? $order->discount : 0 }}</td>
+                              <td colspan="5" align="left">Discount: Rs. {{ $order->discount ? $order->discount : 0 }}</td>
                             </tr>
                             <tr>
-                              <td colspan="5" align="right">Amount Paid: Rs. {{ $order->payable_amount }}</td>
+                              <td colspan="5" align="left">Amount Paid: Rs. {{ $order->payable_amount }}</td>
                             </tr>
 
                       </tbody>
