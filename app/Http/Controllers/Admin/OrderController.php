23c25c37b71e77f2b5008ea9c9b3df1fe89ac014
admin <ossimplementers@gmail.com>
2018-11-02 11:55:55 +0530

    ...

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -10,9 +10,6 @@ use App\Order as Order;
 class OrderController extends Controller
 {
     //
-
-    $orders = Order::all();
-
     /**
      * Display a listing of the resource.
      *
@@ -20,7 +17,6 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
         $orders = Order::all();
         return view('admin.pages.order.index', compact('orders'))->with('i', (request()->input('page', 1) - 1) * 10);
     }
