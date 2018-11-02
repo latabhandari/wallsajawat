0e069bad23eb8c0d42a344a344cb200559fd9273
admin <ossimplementers@gmail.com>
2018-11-02 16:16:11 +0530

    ...

--- resources/views/admin/pages/role/edit.blade.php
+++ resources/views/admin/pages/role/edit.blade.php
@@ -101,20 +101,3 @@
       <!-- /.row -->
  </section>
 @stop
-
-@section('pagejs')
-
-    <script src="{{ URL::asset('admin/bower_components/ckeditor/ckeditor.js') }}"></script>
-    <!-- Bootstrap WYSIHTML5 -->
-    <script src="{{ URL::asset('admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
-    <script>
-     $(function () {
-      // Replace the <textarea id="editor1"> with a CKEditor
-      // instance, using default configuration.
-      CKEDITOR.replace('editor1')
-         //bootstrap WYSIHTML5 - text editor
-       $('.textarea').wysihtml5()
-   })
-</script>
-
-@stop
