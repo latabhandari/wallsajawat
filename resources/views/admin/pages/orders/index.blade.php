0d7d9b7deb448239d6bb261da2f37920cea65ba1
admin <ossimplementers@gmail.com>
2018-11-02 13:17:14 +0530

    ...

--- resources/views/admin/pages/orders/index.blade.php
+++ resources/views/admin/pages/orders/index.blade.php
@@ -12,7 +12,7 @@
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Categories</h3>
+              <h3 class="box-title">Orders</h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
