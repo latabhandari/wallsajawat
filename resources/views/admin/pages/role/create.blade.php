2bb0ec8046c25c3eec41e526a2958de122fb9465
admin <ossimplementers@gmail.com>
2018-11-02 15:36:56 +0530

    ....

--- resources/views/admin/pages/role/create.blade.php
+++ resources/views/admin/pages/role/create.blade.php
@@ -22,11 +22,11 @@
                                 <p><i class="fa fa-exclamation-circle"></i> {{ $error }} </p>
                             @endforeach
                     </div>
-                    @endif
+               @endif
                     
-                    @if(Session::has('error'))
-                    <div class="alert-box success">
-                      <h4 class='error'>{{ Session::get('error') }}</h2>
+               @if(Session::has('error'))
+                    <div class="alert-danger alert">
+                        <p><i class="fa fa-exclamation-circle"></i> {{ Session::get('error') }} </p>
                     </div>
                @endif
 
