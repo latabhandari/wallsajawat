b7126009765f61ccc6b786b4e720cbbd78ae0e02
admin <ossimplementers@gmail.com>
2018-11-03 12:09:45 +0530

    ...

--- resources/views/admin/pages/orders/show.blade.php
+++ resources/views/admin/pages/orders/show.blade.php
@@ -30,7 +30,7 @@ ul li{list-style-type:none;padding-left:10px}
                       <div class="col-sm-12 paddingLeftRght0">
                         <div class="order-date-sec">
 
-                          <div class="col-sm-3">
+                          <div class="col-sm-3" style="padding:0px">
                             <p class="marginZero"><strong>CUSTOMER INFO</strong></p>
                             <p class="placed-date"><i class="fa fa-user"></i>&nbsp;{{ $order->user->name  }}<br /><i class="fa fa-envelope"></i>&nbsp;{{ $order->user->email  }}<br /><i class="fa fa-mobile"></i>&nbsp;{{ $order->user->mobile  }}</p>
                           </div>
