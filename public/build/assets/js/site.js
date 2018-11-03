51de3f99e3a2f5a3d45f50cd176b5a437dea98f4
admin <ossimplementers@gmail.com>
2018-11-03 13:17:59 +0530

    ....

--- public/build/assets/js/site.js
+++ public/build/assets/js/site.js
@@ -21,6 +21,12 @@ $(document).ready(function() {
                                                      }
                 });
 
+                 $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+
                 $("#addwishlist").on('click', function() {
                     let id = parseInt($(this).attr('data-attr'));
 
