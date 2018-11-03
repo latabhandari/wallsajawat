d17c0450a7391670e138c9acc5a38baefe104089
admin <ossimplementers@gmail.com>
2018-11-03 11:43:13 +0530

    ...

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -80,7 +80,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-			'email_token' => bin2hex(openssl_random_pseudo_bytes(30)),
+			      'email_token' => bin2hex(openssl_random_pseudo_bytes(30)),
             'unix_timestamp' => time(),
             'verified' => 1 //ucomment
         ]);
