8d133296a5e59a7c1eea88aa1477713fca4f605c
admin <ossimplementers@gmail.com>
2018-11-05 11:50:08 +0530

    ...

--- resources/views/admin/pages/dashboard/home.blade.php
+++ resources/views/admin/pages/dashboard/home.blade.php
@@ -132,6 +132,7 @@
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/modules/data.js"></script>
 <script src="https://code.highcharts.com/modules/drilldown.js"></script>
+<script src="http://code.highcharts.com/modules/export-data.js"></script>
 <script>
   // Create the chart
 Highcharts.chart('container', {
