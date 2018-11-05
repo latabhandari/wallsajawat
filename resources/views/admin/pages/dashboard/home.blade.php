301a361865384861fd74eaa74f9b78806b667c6d
admin <ossimplementers@gmail.com>
2018-11-05 10:07:02 +0530

    ....

--- resources/views/admin/pages/dashboard/home.blade.php
+++ resources/views/admin/pages/dashboard/home.blade.php
@@ -176,9 +176,41 @@ Highcharts.chart('container', {
             "name": "Browsers",
             "colorByPoint": true,
             "data": [
-                @php
-                  echo json_encode($order_array)
-                @endphp
+                {
+                    "name": "Chrome",
+                    "y": 62.74,
+                    "drilldown": "Chrome"
+                },
+                {
+                    "name": "Firefox",
+                    "y": 10.57,
+                    "drilldown": "Firefox"
+                },
+                {
+                    "name": "Internet Explorer",
+                    "y": 7.23,
+                    "drilldown": "Internet Explorer"
+                },
+                {
+                    "name": "Safari",
+                    "y": 5.58,
+                    "drilldown": "Safari"
+                },
+                {
+                    "name": "Edge",
+                    "y": 4.02,
+                    "drilldown": "Edge"
+                },
+                {
+                    "name": "Opera",
+                    "y": 1.92,
+                    "drilldown": "Opera"
+                },
+                {
+                    "name": "Other",
+                    "y": 7.62,
+                    "drilldown": null
+                }
             ]
         }
     ],
