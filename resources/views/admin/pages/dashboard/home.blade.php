84330a61a233da9661c8a8ec952b4a62ceb76b1e
admin <ossimplementers@gmail.com>
2018-11-05 11:52:29 +0530

    ...

--- resources/views/admin/pages/dashboard/home.blade.php
+++ resources/views/admin/pages/dashboard/home.blade.php
@@ -129,10 +129,14 @@
 
 
 @section('pagejs')
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/data.js"></script>
-<script src="https://code.highcharts.com/modules/drilldown.js"></script>
-<script src="http://code.highcharts.com/modules/export-data.js"></script>
+
+<script src="http://code.highcharts.com/highcharts.js"></script>
+ 
+<script src="https://code.highcharts.com/highcharts-more.js"></script> 
+ 
+<script src="http://code.highcharts.com/modules/exporting.js"></script>
+ 
+<script src="https://code.highcharts.com/modules/export-data.js"></script>
 <script>
   // Create the chart
 Highcharts.chart('container', {
