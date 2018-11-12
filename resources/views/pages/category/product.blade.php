1b8ef805b690531772425beac0136d34e0810e17
admin <ossimplementers@gmail.com>
2018-11-12 15:24:58 +0530

    ....

--- resources/views/pages/category/product.blade.php
+++ resources/views/pages/category/product.blade.php
@@ -92,9 +92,9 @@
                     <span class="righttxt">
                        <a href="javascript:void(0)" class="share" data-attr="{{ $product->id }}"><i class="fa fa-share-alt"></i></a>
                        @if(in_array($product->id, $wishlist_pid))
-                          <i class="fa fa-star">dfgdfg</i>
+                          <i class="fa fa-star wshlst"></i>
                        @else
-                          <a href="javascript:void(0)" class="addwishlist" data-attr="{{ $product->id }}"><i class="fa fa-star">dfs</i></a>
+                          <a href="javascript:void(0)" class="addwishlist" data-attr="{{ $product->id }}"><i class="fa fa-star"></i></a>
                        @endif
                     </span> 
                  </div>
