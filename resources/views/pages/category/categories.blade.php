a87224e51720d6a5c7028962e59630f33644e713
admin <ossimplementers@gmail.com>
2018-11-12 14:30:29 +0530

    ...

--- resources/views/pages/category/categories.blade.php
+++ resources/views/pages/category/categories.blade.php
@@ -49,7 +49,6 @@
               <div class="col-sm-3 text-center selling-imgs"> 
                 <div class="box-inner">
                   <a href="{{ route('category.product', $category->slug) }}"><img src="{{ asset('catalog/category/'.$category->wallpaper_image) }}" alt=""></a>
-                 <div class="img-price"> <span class="lefttxt">&nbsp;&nbsp;{{ $category->name }}</span> <span class="righttxt"><i class="fa fa-share-alt"></i><i class="fa fa-star"></i></span> </div>
                </div>
               </div>
           @endforeach
