62f4ce5855affc0cdf3549886648f12ef1f8e227
admin <ossimplementers@gmail.com>
2018-11-26 15:31:31 +0530

    ...

--- app/Http/Middleware/CheckQuantityCart.php
+++ app/Http/Middleware/CheckQuantityCart.php
@@ -27,7 +27,7 @@ class CheckQuantityCart
             $product          =    Product::select('stock_item')->where('id', $product_id)->firstOrFail();
             $stock_item       =    (int) $product->stock_item;
             if ($quantity > $stock_item)
-            return redirect()->back()->with('error_msg', 'Sorry. only '.$stock_item.' Quantity are left');
+            return redirect()->back()->with('error_msg', 'Sorry. Only '.$stock_item.' Quantity(s) are left');
 
           endforeach;
 
