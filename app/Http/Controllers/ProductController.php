76775ec081d44261f5129e70f5746135de35b5bd
admin <ossimplementers@gmail.com>
2018-11-26 13:28:19 +0530

    ...

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -184,7 +184,10 @@ class ProductController extends Controller
            $request->session()->forget('discount');
         /* close delete the coupon discount */
 
-        print_r(Cart::content());
+$ff = Cart::content();
+
+echo $ff['162e973400649a184c65705ca4c8d118']->id;
+       // print_r(Cart::content());
 
      	  return view('pages.cart.cart');
      }
