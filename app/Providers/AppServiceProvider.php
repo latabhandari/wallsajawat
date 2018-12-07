1c5868602fca6f8282adaa7b02c57ea271b78a9e
admin <ossimplementers@gmail.com>
2018-12-07 15:48:37 +0530

    ....

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
+use Validator;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
